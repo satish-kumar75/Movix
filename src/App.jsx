71e4ea7d17264c88a7f65e380edc6427dd91b7e7
satsihkumar75 <Satishk404456@gmail.com>
2024-06-05 01:15:04 +0530

    Added trending Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
@@ -41,7 +41,7 @@ const App = () => {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      {/* <Footer/> */}
+      <Footer />
     </BrowserRouter>
   );
 };
